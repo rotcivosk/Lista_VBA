9a27e6b014e21e2ded37f73c9addfb326b1282cb
Rotcivosk <46512650+rotcivosk@users.noreply.github.com>
2024-03-27 19:17:39 -0400

    Criada calculadora básica

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+def somar(a,b):
+    return a + b
+def subtrair(a,b):
+    return a - b
+def multiplicar (a,b): 
+    return a * b
+def dividir(a,b):
+    if b == 0:
+        raise ValueError("Dividir por 0, não, né")
+    return a / b
+def potencia(a,b):
+    return a^b
+
+    
